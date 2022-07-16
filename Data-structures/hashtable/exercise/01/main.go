76d0d89934af7d13d1a7de55fb00cd2b401914e9
chibuikeIg <paulchibiukeigweze@gmail.com>
2022-07-16 20:32:26 +0100

    hashtable exercise init commit

---
+++ Data-structures/hashtable/exercise/01/main.go
@@ -0,0 +1,10 @@
+package main
+
+// Excercise: Find First Recurring character
+
+// Brute force solution: I will assume the input will be an array or slice of just integers
+
+func main() {
+
+	// fmt.Println(char)
+}
