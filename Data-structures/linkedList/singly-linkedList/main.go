0179a3a2150d8ed5449c254b0dff807e3c7cd3ec
chibuikeIg <paulchibiukeigweze@gmail.com>
2022-07-17 17:54:43 +0100

    linkedlist implementation: insert

--- Data-structures/linkedList/singly-linkedList/main.go
+++ Data-structures/linkedList/singly-linkedList/main.go
@@ -8,15 +8,16 @@ type Node struct {
 }
 
 type LinkedList struct {
-	data *Node
-	end  *Node
+	data   *Node
+	end    *Node
+	length int
 }
 
 func NewLinkedList(v int) *LinkedList {
 
 	data := Node{value: v}
 
-	return &LinkedList{&data, &data}
+	return &LinkedList{&data, &data, 1}
 }
 
 func (ll *LinkedList) Append(v int) {
@@ -27,6 +28,10 @@ func (ll *LinkedList) Append(v int) {
 
 	ll.end = &newNode
 
+	ll.length += 1
+
+	return
+
 }
 
 func (ll *LinkedList) Prepend(v int) {
@@ -34,6 +39,48 @@ func (ll *LinkedList) Prepend(v int) {
 	newNode := Node{value: v, next: ll.data}
 
 	ll.data = &newNode
+
+	ll.length += 1
+
+	return
+}
+
+func (ll *LinkedList) Insert(index, v int) {
+
+	// don't iterate if insertion is for the first or the end of the list
+
+	if index == 0 {
+		ll.Prepend(v)
+		return
+	} else if index == ll.length {
+		ll.Append(v)
+		return
+	}
+
+	newNode := Node{value: v}
+
+	leader := ll.transverse(index - 1)
+	holdPointer := leader.next
+	leader.next = &newNode
+	newNode.next = holdPointer
+	ll.length += 1
+
+	return
+
+}
+
+func (ll *LinkedList) transverse(index int) *Node {
+
+	counter := 0
+
+	currentNode := ll.data
+
+	for counter != index {
+		currentNode = currentNode.next
+		counter++
+	}
+
+	return currentNode
 }
 
 func main() {
@@ -46,6 +93,8 @@ func main() {
 
 	newLinkedList.Prepend(2)
 
-	fmt.Println(newLinkedList)
+	newLinkedList.Insert(3, 22)
+
+	fmt.Println(newLinkedList.data.next.next.next)
 
 }
