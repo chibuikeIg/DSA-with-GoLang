4197ec6f245b001d1eca776216726efe3dc3f82b
chibuikeIg <paulchibuikeigweze@gmail.com>
2022-09-04 16:59:43 +0100

    BST implementation

---
+++ Data-structures/Trees/binary-tree/bst/main.go
@@ -0,0 +1,82 @@
+package main
+
+import "fmt"
+
+type Node struct {
+	value int
+	right *Node
+	left  *Node
+}
+
+type BinarySearchTree struct {
+	root   *Node
+	length int
+}
+
+func NewBinarySearchTree() *BinarySearchTree {
+	return &BinarySearchTree{}
+}
+
+func (bst *BinarySearchTree) insert(value int) {
+
+	if bst.length == 0 {
+		bst.root = &Node{value: value}
+	}
+
+	bst.addByTraverse(value)
+
+	bst.length += 1
+
+}
+
+func (bst *BinarySearchTree) addByTraverse(value int) {
+
+	currentNode := bst.root
+
+	counter := 0
+
+	for counter != bst.length {
+
+		if currentNode.value < value && currentNode.right == nil {
+
+			currentNode.right = &Node{value: value}
+			break
+
+		} else if currentNode.value < value && currentNode.right != nil {
+
+			currentNode = currentNode.right
+
+		}
+
+		if currentNode.value > value && currentNode.left == nil {
+
+			currentNode.left = &Node{value: value}
+			break
+
+		} else if currentNode.value > value && currentNode.left != nil {
+
+			currentNode = currentNode.left
+
+		}
+
+		counter++
+
+	}
+
+}
+
+func main() {
+
+	newBST := NewBinarySearchTree()
+
+	newBST.insert(3)
+
+	newBST.insert(5)
+
+	newBST.insert(2)
+
+	newBST.insert(4)
+
+	fmt.Println(newBST.root.right.left.value)
+
+}
